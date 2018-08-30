e68e4c729512d74cb8849787b6c174b1adaf1174
Dima Shestakov <dmitriys9701@yandex.ru>
2018-08-30 21:25:58 +0300

    MenuWithRouter - added links

--- app/components/MenuWithRouter/index.js
+++ app/components/MenuWithRouter/index.js
@@ -1,35 +1,46 @@
 import React from 'react';
-import { Link, NavLink , withRouter } from 'react-router-dom';
+import { Link, NavLink, withRouter } from 'react-router-dom';
 import { Menu, Icon } from 'antd';
 
 const MenuWithRouter = props => {
-  console.log(props)
+  console.log(props);
   return (
-    <Menu selectedKeys={props.currentLocation} activeKey={props.currentLocation} theme="dark" mode="inline" defaultSelectedKeys={['']}>
+    <Menu
+      selectedKeys={[props.currentLocation]}
+      activeKey={props.currentLocation}
+      theme="dark"
+      mode="inline"
+      defaultSelectedKeys={['']}
+    >
+      <Menu.Item key="/userprofile">
+        <Icon type="user" />
+        <span className="nav-text">Мой профиль</span>
+        <Link to="/userprofile" />
+      </Menu.Item>
       <Menu.Item key="/catalog">
         <Icon type="table" />
         <span className="nav-text">Справочники</span>
-        <Link to="/catalog"></Link>
+        <Link to="/catalog" />
       </Menu.Item>
-      <Menu.Item key="/profile">
+      <Menu.Item key="/organisationprofile">
         <Icon type="home" />
         <span className="nav-text">Профиль организации</span>
-        <Link to="/profile" ></Link>
+        <Link to="/organisationprofile" />
       </Menu.Item>
       <Menu.Item key="/settings">
         <Icon type="setting" />
         <span className="nav-text">Параметры системы</span>
-        <NavLink to="/settings"></NavLink>
+        <NavLink to="/settings" />
       </Menu.Item>
       <Menu.Item key="/roles">
         <Icon type="upload" />
         <span className="nav-text">Роли и разрешения</span>
-        <NavLink to="/roles"></NavLink>
+        <NavLink to="/roles" />
       </Menu.Item>
       <Menu.Item key="/employees">
         <Icon type="team" />
         <span className="nav-text">Сотрудники</span>
-        <NavLink to="/employees"></NavLink>
+        <NavLink to="/employees" />
       </Menu.Item>
     </Menu>
   );
