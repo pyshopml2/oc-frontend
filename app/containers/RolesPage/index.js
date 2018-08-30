ae1eb6f81a71c1e1eef8f2068573135ddd68c5f9
Dima Shestakov <dmitriys9701@yandex.ru>
2018-08-31 00:19:44 +0300

    RolesPage - added table with checkboxes

--- app/containers/RolesPage/index.js
+++ app/containers/RolesPage/index.js
@@ -10,6 +10,7 @@ import { connect } from 'react-redux';
 import { FormattedMessage } from 'react-intl';
 import { createStructuredSelector } from 'reselect';
 import { compose } from 'redux';
+import { Table, Checkbox } from 'antd';
 
 import injectSaga from 'utils/injectSaga';
 import injectReducer from 'utils/injectReducer';
@@ -19,10 +20,88 @@ import saga from './saga';
 import messages from './messages';
 import BasicLayout from '../BasicLayout/Loadable';
 
+const roles = [
+  'Системный администратор',
+  'Руководитель отдела продаж',
+  'Бухгалтер',
+  'Менеджер продаж',
+  'Трейд-маркетолог',
+  'Начальник склада',
+];
+
+const columns = [
+  {
+    title: '',
+    dataIndex: 'name',
+    key: 'name',
+  },
+  {
+    title: 'Просмотр всех клиентов',
+    dataIndex: 'canViewAllClients',
+    key: 'canViewAllClients',
+    align: 'center',
+  },
+  {
+    title: 'Просмотр только своих клиентов',
+    dataIndex: 'canViewOnlyOwnClients',
+    key: 'canViewOnlyOwnClients',
+    align: 'center',
+  },
+  {
+    title: 'Просмотр всех задач',
+    dataIndex: 'canViewAllTasks',
+    key: 'canViewAllTasks',
+    align: 'center',
+  },
+  {
+    title: 'Просмотр только своих задач',
+    dataIndex: 'canViewOnlyOwnTasks',
+    key: 'canViewOnlyOwnTasks',
+    align: 'center',
+  },
+  {
+    title: 'Добавление клиента',
+    dataIndex: 'canAddClient',
+    key: 'canAddClient',
+    align: 'center',
+  },
+  {
+    title: 'Редактирование клиента',
+    dataIndex: 'canEditClient',
+    key: 'canEditClient',
+    align: 'center',
+  },
+];
+
+const dataSourse = [];
+
+const row = [];
+
+columns.forEach(column => {
+  if (column.dataIndex !== 'name') {
+    row[column.dataIndex] = <Checkbox />;
+  }
+});
+
+roles.forEach(role => {
+  dataSourse.push({ name: role, ...row });
+});
+
+console.log(dataSourse);
+
 /* eslint-disable react/prefer-stateless-function */
 export class RolesPage extends React.Component {
   render() {
-    return <BasicLayout />;
+    return (
+      <BasicLayout>
+        <Table
+          dataSource={dataSourse}
+          columns={columns}
+          style={{ margin: '30px' }}
+          pagination={false}
+        />
+      </BasicLayout>
+    );
   }
 }
 
