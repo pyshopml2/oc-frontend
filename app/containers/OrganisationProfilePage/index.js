61110129dc81b3cee8d9aeadfecfb73792126c73
Damshka <dskozhevnikova@gmail.com>
2018-09-15 17:11:01 +0300

    Organization Profile changes, User Profile changes

--- app/containers/OrganisationProfilePage/index.js
+++ app/containers/OrganisationProfilePage/index.js
@@ -18,13 +18,83 @@ import reducer from './reducer';
 import saga from './saga';
 import messages from './messages';
 import BasicLayout from '../BasicLayout/Loadable';
+import {Button, Card, Tooltip, List, Input, Icon, Modal} from "antd";
+
+const timeZone = new Date().getTimezoneOffset();
+
+function info() {
+  Modal.info({
+    title: 'Данные успешно внесены!',
+    onOk() {
+    },
+  });
+}
+
+const data = [
+  {
+    title: 'Название',
+    body: 'БСК',
+  },
+  {
+    title: 'Почтовый индекс',
+    body: '192000',
+  },
+  {
+    title: 'Почтовый адрес',
+    body: 'Невский пр. 105',
+  },
+  {
+    title: 'Населенный пункт',
+    body: (
+      <div>
+        <Input style={{ marginBottom: 16, width: '90%' }}/>
+          <Tooltip title="Сохранить">
+            <Button icon="check" style={{ margin: '0 20px' }} onClick={info}/>
+          </Tooltip>
+      </div>
+    ),
+  },
+  {
+    title: 'Регион, область',
+    body: (
+      <div>
+        <Input style={{ marginBottom: 16, width: '90%' }}/>
+          <Tooltip title="Сохранить">
+            <Button icon="check" style={{ margin: '0 20px' }} onClick={info}/>
+          </Tooltip>
+      </div>
+    ),
+  },
+  {
+    title: 'Часовой пояс',
+    body: 'GMT ' + timeZone/60 + ' hours ' + timeZone%60 + ' minutes',
+  },
+  {
+    title: 'Сайт компании',
+    body: 'mysite.com',
+  },
+];
 
 /* eslint-disable react/prefer-stateless-function */
 export class OrganisationProfilePage extends React.Component {
   render() {
     return (
       <BasicLayout>
-        <FormattedMessage {...messages.header} />
+        <div style={{ margin: '15px' }}>
+          <Card title="Профиль организации" loading={false}>
+            <List
+              dataSource={data}
+              renderItem={item => (
+                <List.Item>
+                  <List.Item.Meta
+                    title={<strong>{item.title}:</strong>}
+                    description = {item.body}
+                  />
+                </List.Item>
+              )}
+            />
+          </Card>
+        </div>
       </BasicLayout>
     );
   }
--- app/containers/UserProfilePage/index.js
+++ app/containers/UserProfilePage/index.js
@@ -10,7 +10,7 @@ import { connect } from 'react-redux';
 import { FormattedMessage } from 'react-intl';
 import { createStructuredSelector } from 'reselect';
 import { compose } from 'redux';
-import { Card } from 'antd';
+import {Card, List, Tooltip, Button, Modal, Input} from 'antd';
 
 import injectSaga from 'utils/injectSaga';
 import injectReducer from 'utils/injectReducer';
@@ -20,14 +20,63 @@ import saga from './saga';
 import messages from './messages';
 import BasicLayout from '../BasicLayout/Loadable';
 
+const timeZone = new Date().getTimezoneOffset();
+
+const data = [
+  {
+    title: 'ФИО',
+    body: 'Иванов Иван Иванович',
+  },
+  {
+    title: 'Дата рождения',
+    body: '01/01/1991',
+  },
+  {
+    title: 'Email',
+    body: '1@1.com',
+  },
+  {
+    title: 'Часовой пояс',
+    body: 'GMT ' + timeZone/60 + ' hours ' + timeZone%60 + ' minutes',
+  },
+];
+
+function showModal() {
+  Modal.confirm({
+    title: 'Сменить пароль',
+    content: (
+      <div>
+        <Input addonBefore="Новый пароль" style={{ marginBottom: 16, marginTop: 16 }}/>
+        <Input addonBefore="Повторите пароль" style={{ marginBottom: 16 }} />
+      </div>
+    ),
+    onOk() {},
+    onCancel() {},
+  });
+}
+
 /* eslint-disable react/prefer-stateless-function */
 export class UserProfilePage extends React.Component {
   render() {
     return (
       <BasicLayout>
         <div style={{ margin: '15px' }}>
-          <Card title="Профиль" loading={false}>
-            content
+          <Card title="Профиль пользователя" extra = {
+            <Tooltip title="Изменить пароль">
+              <Button icon="dashboard" style={{ margin: '0 20px' }} onClick={showModal} />
+            </Tooltip>
+          } loading={false}>
+            <List
+              dataSource={data}
+              renderItem={item => (
+                <List.Item>
+                  <List.Item.Meta
+                    title={<strong>{item.title}:</strong>}
+                    description = {item.body}
+                  />
+                </List.Item>
+              )}
+            />
           </Card>
         </div>
       </BasicLayout>
