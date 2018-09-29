e0204bf735f88225cf35115dcdb0871643d590b1
Dima Shestakov <dmitriys9701@yandex.ru>
2018-09-29 13:11:25 +0300

    dev - add menuWithRouter wrapper for page access control

--- app/components/MenuWithRouter/index.js
+++ app/components/MenuWithRouter/index.js
@@ -2,47 +2,98 @@ import React from 'react';
 import { Link, NavLink, withRouter } from 'react-router-dom';
 import { Menu, Icon } from 'antd';
 
-const MenuWithRouter = props => {
-  return (
-    <Menu
-      selectedKeys={[props.currentLocation]}
-      activeKey={props.currentLocation}
-      theme="dark"
-      mode="inline"
-      defaultSelectedKeys={['']}
-    >
-      <Menu.Item key="/userprofile">
-        <Icon type="user" />
-        <span className="nav-text">Личный профиль</span>
-        <Link to="/userprofile" />
-      </Menu.Item>
-      <Menu.Item key="/catalog">
-        <Icon type="table" />
-        <span className="nav-text">Справочники</span>
-        <Link to="/catalog" />
-      </Menu.Item>
-      <Menu.Item key="/organisationprofile">
-        <Icon type="home" />
-        <span className="nav-text">Профиль организации</span>
-        <Link to="/organisationprofile" />
-      </Menu.Item>
-      <Menu.Item key="/settings">
-        <Icon type="setting" />
-        <span className="nav-text">Параметры системы</span>
-        <NavLink to="/settings" />
-      </Menu.Item>
-      <Menu.Item key="/roles">
-        <Icon type="upload" />
-        <span className="nav-text">Роли и разрешения</span>
-        <NavLink to="/roles" />
-      </Menu.Item>
-      <Menu.Item key="/employees">
-        <Icon type="team" />
-        <span className="nav-text">Сотрудники</span>
-        <NavLink to="/employees" />
-      </Menu.Item>
-    </Menu>
-  );
-};
+const adminRoutes = [
+  {
+    link: 'userprofile',
+    label: 'Личный профиль',
+    icon: 'user',
+  },
+  {
+    link: 'catalog',
+    label: 'Справочники',
+    icon: 'table',
+  },
+  {
+    link: 'organisationprofile',
+    label: 'Профиль организации',
+    icon: 'home',
+  },
+  {
+    link: 'settings',
+    label: 'Параметры системы',
+    icon: 'setting',
+  },
+  {
+    link: 'roles',
+    label: 'Роли и разрешения',
+    icon: 'upload',
+  },
+  {
+    link: 'employees',
+    label: 'Сотрудники',
+    icon: 'team',
+  },
+];
+
+const userRoutes = [
+  {
+    link: 'userprofile',
+    label: 'Личный профиль',
+    icon: 'user',
+  },
+  {
+    link: 'clients',
+    label: 'Клиенты',
+    icon: 'shopping-cart',
+  },
+  {
+    link: 'tasks',
+    label: 'Задачи',
+    icon: 'project',
+  },
+  {
+    link: 'employees',
+    label: 'Список сотрудников',
+    icon: 'team',
+  },
+];
+
+const routesWithRoles = [
+  {
+    routes: adminRoutes,
+    role: 'admin',
+  },
+  {
+    routes: userRoutes,
+    role: 'user',
+  },
+];
+
+const roles = ['admin', 'user'];
+
+const renderMenuItems = (routeswithroles, roles, currentRole) =>
+  routeswithroles.map(item => {
+    if (item.role === currentRole) {
+      return item.routes.map(route => (
+        <Menu.Item key={`/${route.link}`}>
+          <Icon type={route.icon} />
+          <span className="nav-text">{route.label}</span>
+          <Link to={`/${route.link}`} />
+        </Menu.Item>
+      ));
+    }
+  }); // TODO: render only once
+
+const MenuWithRouter = props => (
+  <Menu
+    selectedKeys={[props.currentLocation]}
+    activeKey={props.currentLocation}
+    theme="dark"
+    mode="inline"
+    defaultSelectedKeys={['']}
+  >
+    {renderMenuItems(routesWithRoles, roles, props.isAdmin ? 'admin' : 'user')}
+  </Menu>
+);
 
 export default MenuWithRouter;
